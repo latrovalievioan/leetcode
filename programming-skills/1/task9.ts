50d22fc431cf51bec30c720cb183feaa60c565dd
Ioan Latrovaliev <latrovalievioan@gmail.com>
2023-02-16 15:28:03 +0200

    better solution to task9

--- programming-skills/1/task9.ts
+++ programming-skills/1/task9.ts
@@ -1,25 +1,9 @@
-function isHappy(n: number): boolean {
-  const visited = new Set<number>();
-
-  let is = false;
-
-  const sumDigits = (n: number): void => {
-    const digits = `${n}`.split('').map(Number);
-    const sum = digits.reduce((acc, curr) => acc + curr ** 2, 0);
-    if (sum === 1) {
-      is = true;
-      return;
-    } else if (visited.has(sum)) {
-      is = false;
-      return;
-    }
-    visited.add(sum);
-    sumDigits(sum);
-  };
-
-  sumDigits(n);
-
-  return is;
-}
+const isHappy = (n: number, visited: Set<number> = new Set()): boolean => {
+  const digits = `${n}`.split('').map(Number);
+  const sum = digits.reduce((acc, curr) => acc + curr ** 2, 0);
+  if (visited.has(sum)) return false;
+  visited.add(sum);
+  return sum === 1 || isHappy(sum, visited);
+};
 
 console.log(isHappy(19));
